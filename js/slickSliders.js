8deebe69f984d2ede1aebdb93874da427ccd79ea
zdepodany <zdepodany@gmail.com>
2022-06-02 18:57:27 +0200

    Add files

---
+++ js/slickSliders.js
@@ -0,0 +1,25 @@
+// Slick Slider for BG
+$(document).ready(function () {
+  $(".welcomeSlideshow").slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 2000,
+    fade: true,
+    cssEase: "linear",
+    arrows: false,
+    draggable: false,
+  });
+});
+
+// Slick Slider for quotes
+$(document).ready(function () {
+  $(".quoteSlider").slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 2000,
+    fade: true,
+    cssEase: "ease",
+    arrows: false,
+    draggable: true,
+  });
+});
